89f1719b0089cdde4fa8d9b6239c37820b671b7f
JezrrelValles <jezrrel.valles@ia.center>
2023-06-08 21:23:17 -0600

    Commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab28601c7888f446"; }
+/******/ 		__webpack_require__.h = function() { return "0ef2c21ba3077525"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
